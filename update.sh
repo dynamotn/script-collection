60cdf8c3966da110304a7c950e3142d7eb3d161c
Trần Đức Nam <dynamo.foss@gmail.com>
2020-11-08 05:32:09 +0700

    Update existed commands

--- update.sh
+++ update.sh
@@ -4,7 +4,11 @@ BIN_DIR=$(dirname "$(readlink -f "$0")")
 _update() {
   # $1: URL of file
   # $2: File name of command
+  # $3: True/False for add command to gitignore
   curl -SL $1 -o $BIN_DIR/$2 && chmod +x $BIN_DIR/$2
+  if [ "$3" = true ]; then
+    grep -r "$2" $BIN_DIR/.gitignore || echo "$2" >> .gitignore
+  fi
 }
 
 _update_github_raw() {
@@ -24,16 +28,19 @@ _update_github_release() {
   # $3: File name in Github Release URLs
   # $4: Output file name of command
   test -z "$2" && version=$(curl -sSL https://api.github.com/repos/$1/releases/latest | grep -Po "tag_name\": \"(\K.*)(?=\",)") || version=$2
-  _update "https://github.com/$1/releases/download/$version/$3" "$4"
+  _update "https://github.com/$1/releases/download/$version/$3" "$4" true
 }
 
 _run() {
+  pids=""
+
   # Update command by view RAW of file
   is_first_file=true
   while IFS=, read -r repo branch git_path file_name
   do
     $is_first_file && is_first_file=false && continue
-    _update_github_raw "$repo" "$branch" "$git_path" "$file_name"
+    _update_github_raw "$repo" "$branch" "$git_path" "$file_name" &
+    pids+=" $!"
   done < $BIN_DIR/data_raw.csv
 
   # Update command by get release file
@@ -41,8 +48,13 @@ _run() {
   while IFS=, read -r repo version asset_file file_name
   do
     $is_first_file && is_first_file=false && continue
-    _update_github_release "$repo" "$version" "$asset_file" "$file_name"
+    _update_github_release "$repo" "$version" "$asset_file" "$file_name" &
+    pids+=" $!"
   done < $BIN_DIR/data_release.csv
+
+  for pid in $pids; do
+    wait $pid
+  done
 }
 
 _run
