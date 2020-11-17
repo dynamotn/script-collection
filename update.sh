eb4dedf0cbb00ed365e4fe1dd42fdb5b3e30ad34
Trần Đức Nam <dynamo.foss@gmail.com>
2020-11-17 17:07:17 +0700

    Add option to get only one tool

--- update.sh
+++ update.sh
@@ -15,7 +15,7 @@ _update() {
     return
   fi
   if [ "$3" = true ]; then
-    grep -r "$2" $BIN_DIR/.gitignore || echo "/$2" >> .gitignore
+    grep -qr "$2" $BIN_DIR/.gitignore || echo "/$2" >> .gitignore
   fi
   if [ "$4" = true ]; then
     local temp=$(mktemp)
@@ -34,6 +34,7 @@ _update_github_raw() {
   test -z "$2" && branch="master" || branch=$2
   test -z "$3" && git_path=${1##*/} || git_path=$3
   test -z "$4" && file_name=${git_path##*/} || file_name=$4
+  echo "Checking $file_name"
   _update "https://raw.githubusercontent.com/$1/$branch/$git_path" "$file_name"
 }
 
@@ -43,6 +44,7 @@ _update_github_release() {
   # $3: File name in Github Release URLs
   # $4: Output file name of command
   # $5: Flag check is install by external file
+  echo "Checking $4"
   test -z "$2" && version=$(curl -sSL https://api.github.com/repos/$1/releases/latest | grep -Po "tag_name\": \"(\K.*)(?=\",)") || version=$2
   _update "https://github.com/$1/releases/download/$version/$(eval "echo $3")" "$4" true "$5"
 }
@@ -51,35 +53,32 @@ _run() {
   pids=""
 
   # Update command by view RAW of file
-  is_first_file=true
-  while IFS=, read -r repo branch git_path file_name
-  do
-    $is_first_file && is_first_file=false && continue
-    _update_github_raw "$repo" "$branch" "$git_path" "$file_name" &
+  tail -n +2 $BIN_DIR/data_raw.csv | grep -is ",$PACKAGE" | while IFS=, read -r repo branch git_path file_name; do
+    _update_github_raw "$repo" "$branch" "$git_path" "$file_name"
     pids+=" $!"
-  done < $BIN_DIR/data_raw.csv
+  done
 
   # Update command by get release file
-  is_first_file=true
-  while IFS=, read -r repo version asset_file file_name external_install
-  do
-    $is_first_file && is_first_file=false && continue
-    _update_github_release "$repo" "$version" "$asset_file" "$file_name" "$external_install" &
+  tail -n +2 $BIN_DIR/data_release.csv | grep -is ",$PACKAGE" | while IFS=, read -r repo version asset_file file_name external_install; do
+    _update_github_release "$repo" "$version" "$asset_file" "$file_name" "$external_install"
     pids+=" $!"
-  done < $BIN_DIR/data_release.csv
+  done
 
   for pid in $pids; do
     wait $pid
   done
 }
 
-while getopts "hn" arg; do
+while getopts ":hns:" arg; do
   case $arg in
     n) # Update only new command
       NEW_ONLY=true
       ;;
     h) # Display help
       _usage
+      ;;
+    s) # Only one specific tool
+      PACKAGE=$OPTARG
       ;;
   esac
 done
